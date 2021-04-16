b7234ce14e3b867451585cba354bad262658d45d
newcy123 <pnew007@hotmail.co.th>
2021-04-16 07:33:09 +0700

    create form front-end

---
+++ NewsDjangoProject/Esportwebsite/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse #1 เพือเอาไปแสดงหน้าเว็บ
+
+# Create your views here.
+#5 render(request,'Esportwebsite/index.html') ใช้ render แทน HttpResponse
+
+def home(request): #โยนข้อมูลจะต้องโยนเป็น dict เสมอ
+    return render(request,'Esportwebsite/home.html') #7 render(request,'Esportwebsite/index.html',mydata) ส่งข้อมูลไปยัง html
+
+def addnews(request):
+    return render(request,'Esportwebsite/addnew.html')
+
+def result(request): # HTTP POST
+    
+    name = request.POST['name_news']
+    detail = request.POST['detail_news']
+    print(name)
+    mydata ={
+        'name_news' : name,
+        'name_detail':detail,
+    }
+
+
+    return render(request,'Esportwebsite/result.html',mydata)
--- NewsDjangoProject/NewsDjangoProject/urls.py
+++ NewsDjangoProject/NewsDjangoProject/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Esportwebsite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home),
+    path('addnews/',views.addnews),
+    path('result/',views.result,name="result")
 ]
